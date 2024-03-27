8a1917e8cf244a7b53e97780b61b0ee5499e6aa8
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-03-27 13:54:46 +0100

    grundläggande typning

---
+++ typning/script.ts
@@ -0,0 +1,41 @@
+//Låser sig till sin typ
+let myName = "Love";
+let myNumber = 8;
+let myBool = false;
+
+//Funkar inte i TS eftersom de är låsta till sin typ
+//myName = 9;
+//myNumber "hej";
+//myBool = 5
+
+//Det går att byta värde då värdet inte är låst
+myName = "Kalle";
+myNumber = 4;
+myBool = true;
+
+//Låst till en array med nummer i
+let myArray = [1, 2, 3, 4, 5, 6, 7];
+
+//Går inte då den är låst till att innehålla nummer
+//myArray.push("hej");
+
+//Låst till att innehålla strängar &/eller nummber
+let mySecondArray = [1, 2, "hej", 4, 5, 6, 7];
+
+mySecondArray.push("tja");
+
+//Går även lägga till t.ex. boolean i arrayen också på samma sätt. Variabeln måste dock fortfarande ha ett array värde.
+
+//Låst till ett projekt som har dessa properties. Varje property är låst till dens typ
+let myObject = {
+    firstName: "Love",
+    lastName: "Karlquist",
+    age: "21",
+    isAwesome: true
+}
+
+//Går inte, låst till att vara sträng
+//myObject.firstName = 8;
+
+//Går inte lägga till fler properties. Låst till de ursprungliga propertiesarna
+//myObject.newProperty = "hej";
